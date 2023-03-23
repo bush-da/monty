7b7615113f4d358d682bd4ce6821c47f01728f66
bush-da <danielsevani12@gmail.com>
2023-03-23 15:18:27 -0400

    prints the string starting at the top of the stack

---
+++ pstr.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * p_str - prints a string
+ * @head: head node
+ * @number: line number
+ * Return: void
+ */
+
+void f_pstr(stack_t **head, unsigned int number)
+{
+	stack_t *temp;
+
+	temp = *head;
+	while (temp)
+	{
+		if (temp->n > 127 || temp->n <= 0)
+			break;
+		printf("%c", temp->n);
+		temp = temp->next;
+	}
+	printf("\n");
+}
