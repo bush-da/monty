689f22a55d025e9a21b6da270ff40742cba3a95e
bush-da <danielsevani12@gmail.com>
2023-03-23 13:54:15 -0400

    add mul function prototype

--- monty.h
+++ monty.h
@@ -77,5 +77,6 @@ void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int number);
 void f_add(stack_t **head, unsigned int number);
 void f_sub(stack_t **head, unsigned int number);
+void f_mul(stack_t **head, unsigned int number);
 
 #endif
