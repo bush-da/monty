0c43c528fa5605b0d2854ee04aec10d0972163a8
bush-da <danielsevani12@gmail.com>
2023-03-23 11:11:58 -0400

    function that doesn't do anything

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_nop - function that do nothing
+ * @head: head node
+ * @number: line number
+ * Return: void
+ */
+
+void f_nop(stack_t **head, unsigned int number)
+{
+	(void)number;
+	(void)head;
+}
