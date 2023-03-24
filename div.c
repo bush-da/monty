c0ee0e6ae02715ba64a253b5662bfd4611d70d7e
bush-da <danielsevani12@gmail.com>
2023-03-24 05:25:48 -0400

    change error message

--- div.c
+++ div.c
@@ -18,7 +18,7 @@ void f_div(stack_t **head, unsigned int number)
 	}
 	if (len < 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", number);
+		fprintf(stderr, "L%d: can't div, stack too short\n", number);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
